a0ac75678f5f346c4b0da41be356c518585348bc
Mu Zhao <zhaomu817@163.com>
2023-09-06 18:44:28 +0300

    add"if age >= 15" statement

--- AgeExercise.java
+++ AgeExercise.java
@@ -1,18 +1,22 @@
 public class AgeExercise {
     public static void main(String[] args) {
 
-        int age = 100;
+        int age = 16;
         if (age > 0 && age < 18) {
             System.out.println("You are underage.");
+                if(age >= 15){
+                    System.out.println("You can drive a moped.");
         }else if(age >= 65){
-         System.out.println("You are retired");
+            System.out.println("You are retired");
         }else{
             System.out.println("You are an adult.");
         }
-
         System.out.println("Press space to exit.");
+
 
         }
     }
+}
 
 
+                    
