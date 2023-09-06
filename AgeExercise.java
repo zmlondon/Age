7da5f2363b32074895767dd1fbc1543bd2d0692c
Mu Zhao <zhaomu817@163.com>
2023-09-06 19:04:59 +0300

    add more age statements

--- AgeExercise.java
+++ AgeExercise.java
@@ -8,9 +8,17 @@ public class AgeExercise {
                     System.out.println("You can drive a moped.");
         }else if(age >= 65){
             System.out.println("You are retired");
+        }else if(age == 20 || age == 30 || age == 40 || age == 50 || age == 60 || age == 70 || age == 80 || age == 90 || age == 100 || age == 110 || age == 120){
+            System.out.println("Anniversary Party!!");
+        }else if(age == 100){
+            System.out.println("congratulations!")
+            System.out.println("congratulations!")
+            System.out.println("congratulations!")
+        }else if(age <= 40 && age >= 50){
+            System.out.println("wish happy mid-life")
         }else{
             System.out.println("You are an adult.");
-        }
+        
         System.out.println("Press space to exit.");
 
 
