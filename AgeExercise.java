ffb4b6036ad54653faf1fd57ca4ff3fca22d35f6
Mu Zhao <zhaomu817@163.com>
2023-09-06 19:28:52 +0300

    typing erros

--- AgeExercise.java
+++ AgeExercise.java
@@ -1,30 +1,26 @@
 public class AgeExercise {
     public static void main(String[] args) {
 
-        int age = 16;
+        int age = 40;
         if (age > 0 && age < 18) {
             System.out.println("You are underage.");
-                if(age >= 15){
-                    System.out.println("You can drive a moped.");
-        }else if(age >= 65){
-            System.out.println("You are retired");
-        }else if(age == 20 || age == 30 || age == 40 || age == 50 || age == 60 || age == 70 || age == 80 || age == 90 || age == 100 || age == 110 || age == 120){
-            System.out.println("Anniversary Party!!");
-        }else if(age == 100){
-            System.out.println("congratulations!")
-            System.out.println("congratulations!")
-            System.out.println("congratulations!")
-        }else if(age <= 40 && age >= 50){
-            System.out.println("wish happy mid-life")
-        }else{
-            System.out.println("You are an adult.");
-        
-        System.out.println("Press space to exit.");
-
-
+            if (age >= 15) {
+                System.out.println("You can drive a moped.");
+            }
+        }
+        if (age >= 65) {
+                System.out.println("You are retired");
+            }
+        if (age % 10 == 0) {
+                System.out.println("Anniversary Party!!");
+            }
+            if (age == 100) {
+                System.out.println("congratulations!");
+                System.out.println("congratulations!");
+                System.out.println("congratulations!");
+            }
+        if (age >= 40 && age <= 50) {
+                System.out.println("wish happy mid-life");
+        } 
         }
     }
-}
-
-
-                    
